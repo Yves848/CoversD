f5ceb7dd32baeab742be3f17bd9f8310b8a7161e
yves.godart@gmail.com <Yves848>
2020-08-14 14:20:32 +0200

    miniplayer

--- uMain.pas
+++ uMain.pas
@@ -6,16 +6,16 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics, System.IOUtils, System.Types,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, uTypes, utags, Vcl.StdCtrls, sPanel, Vcl.ExtCtrls, sSkinManager, sSkinProvider, sButton, Vcl.ComCtrls,
   sTreeView, acShellCtrls, sListView, sComboBoxes, sSplitter, Vcl.Buttons, sSpeedButton, System.ImageList, Vcl.ImgList, acAlphaImageList,
-  acProgressBar, JvComponentBase, JvThread, sMemo, Vcl.Mask, sMaskEdit, sCustomComboEdit, sToolEdit, acImage, JPEG, PNGImage, GIFImg,TagsLibrary;
+  acProgressBar, JvComponentBase, JvThread, sMemo, Vcl.Mask, sMaskEdit, sCustomComboEdit, sToolEdit, acImage, JPEG, PNGImage, GIFImg, TagsLibrary,
+  acNoteBook, sTrackBar, acArcControls, sGauge, BASS;
 
 type
   TfMain = class(TForm)
-    Button1: TButton;
     pnMain: TsPanel;
     sSkinProvider1: TsSkinProvider;
     sSkinManager1: TsSkinManager;
     pnToolbar: TsPanel;
-    sRollOutPanel1: TsRollOutPanel;
+    sROPMedia: TsRollOutPanel;
     sSplitter1: TsSplitter;
     sTVMedias: TsTreeView;
     pnToolbarTreeView: TsPanel;
@@ -26,19 +26,35 @@ type
     thListMP3: TJvThread;
     sDEFolder: TsDirectoryEdit;
     sILTV: TsAlphaImageList;
-    ImageList1: TImageList;
     sImage1: TsImage;
+    sROPPlaylist: TsRollOutPanel;
+    sSplitter2: TsSplitter;
+    TrackBar1: TsTrackBar;
+    sGauge1: TsGauge;
+    Timer1: TTimer;
+    sGauge2: TsGauge;
+    TrackBar2: TsTrackBar;
     procedure Button1Click(Sender: TObject);
     procedure thListMP3Execute(Sender: TObject; Params: Pointer);
     procedure sTVMediasChange(Sender: TObject; Node: TTreeNode);
     procedure sTVMediasExpanding(Sender: TObject; Node: TTreeNode; var AllowExpansion: Boolean);
     procedure sDEFolderAfterDialog(Sender: TObject; var Name: string; var Action: Boolean);
+    procedure sTVMediasKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure TrackBar2Change(Sender: TObject);
+    procedure TrackBar1Change(Sender: TObject);
   private
     { Déclarations privées }
     function findNode(sLabel: String): TTreeNode;
-    procedure ListCoverArts(Tags : TTags);
+    procedure ListCoverArts(Tags: TTags);
   public
     { Déclarations publiques }
+    Channel: HStream;
+    CurrentPlayingFileName: String;
+    AdjustingPlaybackPosition: Boolean;
+    procedure PlayNext;
+    procedure PlayStream(FileName: String);
     procedure addfileName;
     procedure setPBMax;
     procedure SetPBPosition;
@@ -61,6 +77,31 @@ implementation
 
 uses
   JvDynControlEngineVcl;
+
+procedure StreamEndCallback(handle: HSYNC; channel, data: DWORD; user: Pointer); stdcall;
+begin
+
+end;
+
+procedure TFMain.PlayNext;
+var
+    CurrentIndex: Integer;
+    i: Integer;
+begin
+    //* Decide current playing index
+end;
+
+
+procedure TFMain.PlayStream(FileName: String);
+begin
+    Channel := BASS_StreamCreateFile(False, PChar(FileName), 0, 0, BASS_UNICODE OR BASS_STREAM_AUTOFREE);
+    //* Set an end sync which will be called when playback reaches end to play the next song
+    BASS_ChannelSetSync(Channel, BASS_SYNC_END, 0, @StreamEndCallback, 0);
+    CurrentPlayingFileName := FileName;
+    TrackBar1.Max := BASS_ChannelGetLength(Channel, BASS_POS_BYTE);
+    //* Start playback
+    BASS_ChannelPlay(Channel, True);
+end;
 
 procedure TfMain.Button1Click(Sender: TObject);
 var
@@ -117,7 +158,7 @@ begin
     mediaFile := tMediaFile.Create(sFileName);
     aNode := sTVMedias.Items.AddChildObject(aParentNode, sFile, mediaFile);
     aNode.ImageIndex := 0;
-    if mediaFile.tags.CoverArts.Count > 0 then
+    if mediaFile.Tags.CoverArts.Count > 0 then
       aNode.ImageIndex := 1;
     if TDirectory.Exists(sFileName) then
     begin
@@ -166,6 +207,21 @@ begin
 
 end;
 
+procedure TfMain.FormCreate(Sender: TObject);
+var
+    Volume: Double;
+begin
+    //* Never forget to init BASS
+    BASS_Init(-1, 44100, 0, Self.Handle, 0);
+    //* Set VU max. values
+    sGauge1.MaxValue := High(Word) div 2 + 1;
+    sGauge2.MaxValue := High(Word) div 2 + 1;
+    //* Get current volume
+    Volume := BASS_GetVolume;
+    TrackBar2.Position := 100 - Round(Volume * 100);
+
+end;
+
 procedure TfMain.ResetPB;
 begin
   pb1.Position := 0;
@@ -190,82 +246,88 @@ end;
 
 procedure TfMain.SetPBPosition;
 begin
-   pb1.Position := pb1.Position + 1;
+  pb1.Position := pb1.Position + 1;
 end;
 
 procedure TfMain.sTVMediasChange(Sender: TObject; Node: TTreeNode);
 begin
-  //sMemo1.Clear;
+  // sMemo1.Clear;
   if assigned(Node.Data) then
   begin
-    //sMemo1.lines.Add(tMediaFile(Node.Data).tags.filename);
-    //sMemo1.lines.Add(tMediaFile(Node.Data).tags.GetTag('ARTIST'));
-    //sMemo1.lines.Add(tMediaFile(Node.Data).tags.GetTag('TITLE'));
-    //sMemo1.lines.Add(tMediaFile(Node.Data).tags.GetTag('ALBUM'));
-    ListCoverArts(tMediaFile(Node.Data).tags);
+    // sMemo1.lines.Add(tMediaFile(Node.Data).tags.filename);
+    // sMemo1.lines.Add(tMediaFile(Node.Data).tags.GetTag('ARTIST'));
+    // sMemo1.lines.Add(tMediaFile(Node.Data).tags.GetTag('TITLE'));
+    // sMemo1.lines.Add(tMediaFile(Node.Data).tags.GetTag('ALBUM'));
+    ListCoverArts(tMediaFile(Node.Data).Tags);
   end;
 
 end;
 
-procedure TfMain.ListCoverArts(Tags : TTags);
+procedure TfMain.ListCoverArts(Tags: TTags);
 var
-    i: Integer;
-    ImageJPEG: TJPEGImage;
-    ImagePNG: TPNGImage;
-    ImageGIF: TGIFImage;
-    Bitmap: TBitmap;
+  i: Integer;
+  ImageJPEG: TJPEGImage;
+  ImagePNG: TPNGImage;
+  ImageGIF: TGIFImage;
+  Bitmap: TBitmap;
 begin
-    //* List cover arts
-    for i := 0 to Tags.CoverArtCount - 1 do begin
-        Bitmap := TBitmap.Create;
-        try
-            with Tags.CoverArts[i] do begin
-                Stream.Seek(0, soBeginning);
-                case PictureFormat of
-                    tpfJPEG: begin
-                        ImageJPEG := TJPEGImage.Create;
-                        try
-                            ImageJPEG.LoadFromStream(Stream);
-                            Bitmap.Assign(ImageJPEG);
-                        finally
-                            FreeAndNil(ImageJPEG);
-                        end;
-                    end;
-                    tpfPNG: begin
-                        ImagePNG := TPNGImage.Create;
-                        try
-                            ImagePNG.LoadFromStream(Stream);
-                            Bitmap.Assign(ImagePNG);
-                        finally
-                            FreeAndNil(ImagePNG);
-                        end;
-                    end;
-                    tpfGIF: begin
-                        ImageGIF := TGIFImage.Create;
-                        try
-                            ImageGIF.LoadFromStream(Stream);
-                            Bitmap.Assign(ImageGIF);
-                        finally
-                            FreeAndNil(ImageGIF);
-                        end;
-                    end;
-                    tpfBMP: begin
-                        Bitmap.LoadFromStream(Stream);
-                    end;
-                end;
-                sImage1.Picture.Bitmap.Assign(BitMap);
-                //* This function resizes with nearest-neighbour mode (which is not recommended as it gives the worst quality possible),
-                //* if you need a top-quality lanczos resizer which uses Graphics32, please contact me.
-//                ResizeBitmap(Bitmap, 160, 120, clWhite);
-//                with ListView2.Items.Add do begin
-//                    Caption := Tags.CoverArts[i].Name;
-//                    ImageIndex := ImageListThumbs.Add(Bitmap, nil);
-//                end;
+  // * List cover arts
+  for i := 0 to Tags.CoverArtCount - 1 do
+  begin
+    Bitmap := TBitmap.Create;
+    try
+      with Tags.CoverArts[i] do
+      begin
+        Stream.Seek(0, soBeginning);
+        case PictureFormat of
+          tpfJPEG:
+            begin
+              ImageJPEG := TJPEGImage.Create;
+              try
+                ImageJPEG.LoadFromStream(Stream);
+                Bitmap.Assign(ImageJPEG);
+              finally
+                FreeAndNil(ImageJPEG);
+              end;
             end;
-        finally
-            FreeAndNil(Bitmap);
+          tpfPNG:
+            begin
+              ImagePNG := TPNGImage.Create;
+              try
+                ImagePNG.LoadFromStream(Stream);
+                Bitmap.Assign(ImagePNG);
+              finally
+                FreeAndNil(ImagePNG);
+              end;
+            end;
+          tpfGIF:
+            begin
+              ImageGIF := TGIFImage.Create;
+              try
+                ImageGIF.LoadFromStream(Stream);
+                Bitmap.Assign(ImageGIF);
+              finally
+                FreeAndNil(ImageGIF);
+              end;
+            end;
+          tpfBMP:
+            begin
+              Bitmap.LoadFromStream(Stream);
+            end;
         end;
+        sImage1.Picture.Bitmap.Assign(Bitmap);
+        // * This function resizes with nearest-neighbour mode (which is not recommended as it gives the worst quality possible),
+        // * if you need a top-quality lanczos resizer which uses Graphics32, please contact me.
+        // ResizeBitmap(Bitmap, 160, 120, clWhite);
+        // with ListView2.Items.Add do begin
+        // Caption := Tags.CoverArts[i].Name;
+        // ImageIndex := ImageListThumbs.Add(Bitmap, nil);
+        // end;
+      end;
+    finally
+      FreeAndNil(Bitmap);
     end;
+  end;
 end;
 
 procedure TfMain.sTVMediasExpanding(Sender: TObject; Node: TTreeNode; var AllowExpansion: Boolean);
@@ -275,6 +337,23 @@ begin
     aPath := Node.Text;
     aNode := Node;
     thListMP3.Execute(Self);
+  end;
+end;
+
+procedure TfMain.sTVMediasKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+var
+  aNode : tTreeNode;
+  Mgs   : TMsg;
+begin
+  if Key = VK_RETURN then
+  begin
+     aNode := sTVMedias.Selected;
+     if assigned(aNode.Data) then
+     begin
+       BASS_ChannelStop(Channel);
+       PlayStream(tMediaFile(aNode.data).tags.FileName);
+     end;
+     PeekMessage(Mgs,0,WM_CHAR,WM_CHAR,PM_REMOVE);
   end;
 end;
 
@@ -319,7 +398,7 @@ begin
       thListMP3.Synchronize(TfMain(Params).addfileName);
 
     inc(i);
-    //thListMP3.Synchronize(TfMain(Params).SetPBPosition);
+    // thListMP3.Synchronize(TfMain(Params).SetPBPosition);
   end;
   thListMP3.Synchronize(TfMain(Params).ExpandNode);
   thListMP3.Synchronize(TfMain(Params).ResetPB);
@@ -327,9 +406,54 @@ begin
 
 end;
 
+procedure TfMain.Timer1Timer(Sender: TObject);
+var
+    Level: Cardinal;
+    LeftLevel: Word;
+    RightLevel: Word;
+
+begin
+    Level := BASS_ChannelGetLevel(Channel);
+    //* Separate L & R channel
+    LeftLevel := LoWord(Level);
+    RightLevel := HiWord(Level);
+    //* Set the VUs
+    if BASS_ChannelIsActive(Channel) = BASS_ACTIVE_PLAYING then begin
+        //* Logarithmic
+
+            sGauge1.Progress := LeftLevel;
+            sGauge2.Progress := RightLevel;
+
+    end else begin
+        sGauge1.Progress := sGauge1.Progress - 1000;
+        sGauge2.Progress := sGauge2.Progress - 1000;
+    end;
+    //* Playing position
+    AdjustingPlaybackPosition := True;
+    TrackBar1.Position := BASS_ChannelGetPosition(Channel, BASS_POS_BYTE);
+    AdjustingPlaybackPosition := False;
+
+end;
+
+procedure TfMain.TrackBar1Change(Sender: TObject);
+begin
+  if NOT AdjustingPlaybackPosition then begin
+        BASS_ChannelSetPosition(Channel, TrackBar1.Position, BASS_POS_BYTE);
+    end;
+end;
+
+procedure TfMain.TrackBar2Change(Sender: TObject);
+var
+    Volume: Double;
+begin
+    Volume := (100 - TrackBar2.Position) / TrackBar2.Max;
+    BASS_SetVolume(Volume);
+
+end;
+
 procedure TfMain.updateTV;
 begin
-    sTVMedias.Refresh;
+  sTVMedias.Refresh;
 end;
 
 end.
